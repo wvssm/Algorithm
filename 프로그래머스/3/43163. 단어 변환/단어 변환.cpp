792af29488d7b66aa72b706f085e6a4a4a286698
Kang Sumin <neoramiregi@gmail.com>
2024-03-15 11:28:01 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,41 +1,41 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
-int vis[51];
-int min_level = 55;
 
+int vis[51];
+int min_val = 52;
 bool check(string a, string b){
     int cnt = 0;
-    for(int i = 0; i < a.size(); i++){
+    for(int i=0; i<a.size(); i++){
         if(a[i]!=b[i]) cnt++;
     }
-    cout << a+", "+b+"\n";
-    if(cnt <= 1) return true;
+    if(cnt<=1) return true;
     else return false;
 }
 
-void bfs(string begin, string target, vector<string> words, int level){
+void dfs(string begin, string target, vector<string> words, int level){
     if(begin == target){
-        if(min_level>level) min_level = level;
+        if(min_val>level){
+            min_val = level;
+        }
         return;
     }
     
     for(int i=0; i<words.size(); i++){
         if(vis[i]==0){
-            if(check(begin,words[i])){
+            if(check(begin, words[i])){
                 vis[i]=1;
-                bfs(words[i],target,words,level+1);
+                dfs(words[i],target,words,level+1);
                 vis[i]=0;
             }
-            else continue;
         }
     }
 }
 
 int solution(string begin, string target, vector<string> words) {
-    bfs(begin,target,words,0);   
-    if(min_level == 55) return 0;
-    else return min_level;
+    int answer = 0;
+    dfs(begin,target,words,0);
+    if(min_val == 52) return 0;
+    else return min_val;
 }
