90474fe13a18ed71db59e4cfa6da10937d2d71bd
Kang Sumin <neoramiregi@gmail.com>
2024-03-15 10:59:02 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.28 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -2,35 +2,36 @@
 #include<queue>
 using namespace std;
 
-int vis[101][101];
+
+int vis[102][102];
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-void bfs(vector<vector<int>> maps){
-    queue<pair<int,int>> q;
+void bfs(vector<vector<int> > maps){
+    queue<pair<int,int>>q;
     q.push({0,0});
-    vis[0][0]=1;
+    vis[0][0]=1; // 이거 빼먹음
     
     while(!q.empty()){
-        auto cur = q.front(); q.pop();
+        pair<int,int> cur = q.front(); q.pop();
         
         for(int i=0; i<4; i++){
             int nx = cur.first + dx[i];
             int ny = cur.second + dy[i];
             
-            if(nx<0 || nx>=maps.size() || ny<0 || ny>=maps[0].size()) continue;
-            if(vis[nx][ny]>0 || maps[nx][ny]==0) continue;
+            if(nx<0||nx>=maps.size()||ny<0||ny>=maps[0].size()) continue;
+            if(maps[nx][ny]==0 || vis[nx][ny]>0) continue;
             
-            vis[nx][ny] = vis[cur.first][cur.second] + 1;
+            vis[nx][ny] = vis[cur.first][cur.second]+1;
             q.push({nx,ny});
         }
     }
 }
 
-int solution(vector<vector<int>> maps)
+int solution(vector<vector<int> > maps)
 {
     int answer = 0;
     bfs(maps);
-    if(vis[maps.size()-1][maps[0].size()-1] == 0) return -1;
+    if(vis[maps.size()-1][maps[0].size()-1]==0) return -1;
     else return vis[maps.size()-1][maps[0].size()-1];
 }
