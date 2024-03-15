d8c12a531dafe48b3c04ccd51b9f0b27f808ea62
Kang Sumin <neoramiregi@gmail.com>
2024-03-15 10:16:35 +0900

    [level 2] Title: 타겟 넘버, Time: 0.54 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -2,22 +2,17 @@
 #include <vector>
 
 using namespace std;
-int answer=0;
-int vis[21][2];
-vector<int>dx= {-1, 1};
 
+int dx[2] = {1,-1};
+int answer = 0;
 void dfs(vector<int> numbers, int target, int sum, int level){
     if(level==numbers.size()){
-        if(sum==target) answer++;
+        if(target==sum) answer++;
         return;
     }
     
     for(int i=0; i<2; i++){
-        if(vis[level][i]==0){
-            vis[level][i] = 1;
-            dfs(numbers, target, sum+numbers[level]*dx[i], level+1);
-            vis[level][i] = 0;
-        }
+        dfs(numbers,target,sum+numbers[level]*dx[i],level+1);
     }
 }
 
