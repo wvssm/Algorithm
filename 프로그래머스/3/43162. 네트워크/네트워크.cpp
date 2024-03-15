f8e7f621357c277c8b35dd011cf576971970482a
Kang Sumin <neoramiregi@gmail.com>
2024-03-15 10:37:47 +0900

    [level 3] Title: 네트워크, Time: 0.76 ms, Memory: 4.05 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -2,23 +2,20 @@
 #include <vector>
 
 using namespace std;
-int vis[201];
-
-void dfs(vector<vector<int>> computers,int num){
-    
-    for(int i=0; i<computers[num].size(); i++){
-        if(computers[num][i] ==1 && vis[i]==0){
-            vis[i]=1;
+int vis[202];
+void dfs(vector<vector<int>> computers, int num) {
+    for(int i=0; i<computers.size(); i++){
+        if(vis[i]==0 && computers[num][i]==1){
+            vis[i] = 1;
             dfs(computers,i);
         }
-    }
-    
+    }    
 }
+
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     for(int i=0; i<computers.size(); i++){
         if(vis[i]==0){
-            vis[i]=1;
             dfs(computers,i);
             answer++;
         }
