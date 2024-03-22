d28a155d7afabdea36aaa3101de4ca10d7110170
Kang Sumin <neoramiregi@gmail.com>
2024-03-22 12:48:59 +0900

    [level 1] Title: 최소직사각형, Time: 0.69 ms, Memory: 4.88 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 int solution(vector<vector<int>> sizes) {
     int answer = 0;
+    int w = 0;
+    int h = 0;
+    
     vector<int> max_e;
     vector<int> min_e;
     
     for(int i=0; i<sizes.size(); i++){
-        max_e.push_back(max(sizes[i][0],sizes[i][1]));
-        min_e.push_back(min(sizes[i][0],sizes[i][1]));
+        w = max(w,max(sizes[i][0],sizes[i][1]));
+        h = max(h,min(sizes[i][0],sizes[i][1]));
     }
     
-    int g_max = *max_element(max_e.begin(), max_e.end());
-    int s_max = *max_element(min_e.begin(), min_e.end());
-    
-    return g_max*s_max;
+    return w*h;
 }
