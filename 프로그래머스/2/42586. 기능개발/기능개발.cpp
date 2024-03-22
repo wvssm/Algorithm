6973c868f0dd422ff79fcc279002d1019951a46e
Kang Sumin <neoramiregi@gmail.com>
2024-03-22 16:08:39 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -5,27 +5,26 @@ using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    int g=0;
-    while(progresses.size()){
-        if(progresses[0]>=100) {
-            g++;
+    int g = 0;
+    
+    while(!progresses.empty()){
+        if(progresses[0]>=100){
             progresses.erase(progresses.begin());
             speeds.erase(speeds.begin());
+            g += 1;
         }
+        
         else{
             if(g!=0){
                 answer.push_back(g);
                 g=0;
             }
-            for(int i=0; i<progresses.size();i++){
+            
+            for(int i=0; i<progresses.size(); i++){
                 progresses[i] += speeds[i];
             }
         }
     }
     answer.push_back(g);
-
-    // 대가리가 100될때까지 계속 speed를 더한다
-    // 다음 대가리가 100미만 일때 까지 계속 수를 더한다
-    // 벡터에 삽입한다.
     return answer;
 }
