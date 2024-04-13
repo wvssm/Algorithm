7111a75c1e2fbcf6a003719b0a46041943331607
Kang Sumin <neoramiregi@gmail.com>
2024-04-13 11:19:08 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.cpp
+++ 프로그래머스/1/42862. 체육복/체육복.cpp
@@ -1,49 +1,23 @@
-/*
- * 관전 포인트
- * 1. 오름차순으로 입력이 주어지지 않을 수도 있다.
- * 2. 자기가 체육복이 없으면 자기가 입어야한다. 남한테 빌려줄 수 없다.*/
 
 #include <string>
 #include <vector>
-#include <iostream>
-#include <algorithm>
 
 using namespace std;
-
-bool vis[31];
-
+int student[35];
 int solution(int n, vector<int> lost, vector<int> reserve) {
-    int answer = n-lost.size();
-    sort(lost.begin(),lost.end());
-    sort(reserve.begin(), reserve.end());
-
-    for(int i=0; i<lost.size(); i++){
-        auto cur = find(reserve.begin(),reserve.end(),lost[i]);
-        if(cur!= reserve.end()){
-            vis[lost[i]] = true;
-            answer++;
+    int answer = 0;
+    for(int i : reserve) student[i] += 1;
+    for(int i : lost) student[i] += -1;
+    for(int i = 1; i <= n; i++) {
+        if(student[i] == -1) {
+            if(student[i-1] == 1) 
+                student[i-1] = student[i] = 0;
+            else if(student[i+1] == 1) 
+                student[i] = student[i+1] = 0;
         }
     }
+    for(int i  = 1; i <=n; i++)
+        if(student[i] != -1) answer++;
 
-    for(int i=0; i<lost.size(); i++){
-        if(vis[lost[i]]) continue; // 체육복을 빌렸으면 pass
-        for(int j=0; j<reserve.size(); j++){
-            if(!vis[reserve[j]]){
-                if(reserve[j]-1 == lost[i] || reserve[j]+1 == lost[i]){
-                    vis[reserve[j]]=true;
-                    answer++;
-                    break;
-                }
-            }
-        }
-    }
     return answer;
-}
-
-int main(){
-    int n = 3;
-    vector<int> lost ={3};
-    vector<int> reserve = {1};
-    int answer = solution(n, lost, reserve);
-    cout << answer;
 }
