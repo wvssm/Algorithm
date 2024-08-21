766c31f31ffbfb68be81b234b6d6271b49b814cb
Kang Sumin <neoramiregi@gmail.com>
2024-08-21 21:25:21 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.30 ms, Memory: 4.08 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,38 +1,35 @@
 #include<vector>
 #include<queue>
-
 using namespace std;
 
-int vis[100][100];
-int dx[4] = {0,1,0,-1};
-int dy[4] = {1,0,-1,0};
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+int vis[101][101];
 
-void bfs(vector<vector<int> > maps){
-    queue<pair<int,int>> q;
+int solution(vector<vector<int> > maps)
+{
+    int n = maps.size();
+    int m = maps[0].size();
+    
+    queue<pair<int, int>> q;
     q.push({0,0});
-    vis[0][0] = 1;
+    vis[0][0]=1;
     
     while(!q.empty()){
-        auto cur = q.front(); q.pop();
+        pair<int, int> cur = q.front(); q.pop();
         
         for(int i=0; i<4; i++){
             int nx = cur.first + dx[i];
             int ny = cur.second + dy[i];
             
-            if(nx<0 || nx >= maps.size() || ny<0 || ny>= maps[0].size()) continue;
-            if(maps[nx][ny]==0 || vis[nx][ny]!=0) continue;
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 이 순서가 굉장히 중요
+            if(vis[nx][ny] > 0 || maps[nx][ny] == 0) continue;
             
-            vis[nx][ny] = vis[cur.first][cur.second]+1;
-            q.push({nx,ny});
-        }
-        
+            vis[nx][ny] = vis[cur.first][cur.second] + 1;
+            q.push({nx, ny});
+        } 
     }
-}
-
-int solution(vector<vector<int> > maps)
-{
-    int answer = 0;
-    bfs(maps);
-    if(vis[maps.size()-1][maps[0].size()-1]==0) return -1;
-    else return vis[maps.size()-1][maps[0].size()-1];
+    
+    if(vis[n-1][m-1]==0) return -1;
+    else return vis[n-1][m-1];
 }
