533b020f6844babb2dad467c3652b1d845fce07f
Kang Sumin <neoramiregi@gmail.com>
2024-08-26 11:22:27 +0900

    [level 3] Title: 네트워크, Time: 2.61 ms, Memory: 6.48 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,19 +1,17 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
 
 int vis[201];
 
-void dfs(int past, int me, vector<vector<int>> computers){
-    vis[me] = 1;
-    cout << "나등장" << me;
+void dfs(int past, int now, vector<vector<int>> computers){
+    vis[now]=1;
     for(int i=0; i<computers.size(); i++){
-        if(i == me) continue;
-        if(i == past) continue;
-        if(vis[i]!=1 && computers[me][i] == 1){
-            dfs(me,i,computers);
+        if(i==past) continue;
+        if(i==now) continue;
+        if(vis[i]==0 && computers[now][i]==1){
+            dfs(now, i, computers);
         }
     }
 }
@@ -21,10 +19,11 @@ void dfs(int past, int me, vector<vector<int>> computers){
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     for(int i=0; i<computers.size(); i++){
-        if(vis[i]==1) continue;
-        vis[i]=1;
-        dfs(-1,i,computers);
-        answer++;
+        if(vis[i]==0){
+            vis[i]=1;
+            dfs(0,i,computers);
+            answer++;
+        }
     }
     return answer;
 }
