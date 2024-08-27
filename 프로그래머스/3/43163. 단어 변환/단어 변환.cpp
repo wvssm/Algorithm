f904110e0e19aaa15f0860aaf4f1793e2a0e9d78
Kang Sumin <neoramiregi@gmail.com>
2024-08-27 10:26:06 +0900

    [level 3] Title: 단어 변환, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,38 +1,36 @@
 #include <string>
-#include <iostream>
 #include <vector>
 
 using namespace std;
-int min_cnt = 100;
 int vis[51];
+int min_cnt = 100;
 
 bool isOneDiff(string a, string b){
     int cnt = 0;
     for(int i=0; i<a.size(); i++){
         if(a[i]!=b[i]) cnt++;
     }
-    if(cnt == 1) return true;
+    
+    if(cnt==1) return true;
     else return false;
 }
 
-void dfs(string begin, string target, int cnt, vector<string> words){
-    if(begin == target){
-        if(cnt < min_cnt) min_cnt = cnt;
+void dfs(string begin, string target, vector<string> words, int cnt){
+    if(begin==target){
+        if(cnt<min_cnt) min_cnt = cnt;
         return;
     }
-    
-    for(int i=0; i<words.size(); i++){  
-        if(isOneDiff(begin,words[i]) && vis[i] == 0){
+    for(int i=0; i<words.size(); i++){
+        if(vis[i]==0 && isOneDiff(begin,words[i])){
             vis[i]=1;
-            cout<< begin << "단어 변경" << words[i] << " " << cnt << "\n";
-            dfs(words[i],target,cnt+1,words);
+            dfs(words[i],target,words,cnt+1);
             vis[i]=0;
         }
     }
 }
 
 int solution(string begin, string target, vector<string> words) {
-    dfs(begin,target,0,words);
+    dfs(begin,target,words,0);
     if(min_cnt == 100) return 0;
-    else return min_cnt;
+    return min_cnt;
 }
