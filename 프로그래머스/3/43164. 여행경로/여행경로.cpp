d4b655c7075d7682b8a8d1e4b3dc2a9eba1084d9
Kang Sumin <neoramiregi@gmail.com>
2024-08-28 16:31:15 +0900

    [level 3] Title: 여행경로, Time: 0.08 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.cpp
+++ 프로그래머스/3/43164. 여행경로/여행경로.cpp
@@ -1,7 +1,12 @@
+// 시간 복잡도: 
+// 티켓 정렬: O(NlogN)
+// 백트래킹: O(N!)
+
+// 간선의 개수 10000개 
+// 1초 안에 해결안될 것 같은데 적절한 알고리즘인가?
 #include <string>
 #include <vector>
 #include <algorithm>
-
 
 using namespace std;
 int vis[100000000];
@@ -19,7 +24,9 @@ void dfs(string begin, int cnt, vector<vector<string>> tickets, vector<string> a
         if(vis[i]==0 && tickets[i][0]==begin){
             vis[i]=1;
             answer.push_back(tickets[i][1]);
+            
             dfs(tickets[i][1],cnt+1,tickets,answer);
+            if(found==1) return;
             vis[i]=0;
             answer.pop_back();
         }
@@ -30,6 +37,7 @@ vector<string> solution(vector<vector<string>> tickets) {
     sort(tickets.begin(), tickets.end());
     vector<string> answer;
     answer.push_back("ICN");
+    
     dfs("ICN",0, tickets, answer);
     return answers[0];
 }
