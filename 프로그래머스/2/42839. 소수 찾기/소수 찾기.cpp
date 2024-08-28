c3a33480f406bd87862d341fdc700cdae56b0ad7
Kang Sumin <neoramiregi@gmail.com>
2024-08-28 15:51:47 +0900

    [level 2] Title: 소수 찾기, Time: 3.72 ms, Memory: 3.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,44 +1,51 @@
 #include <string>
 #include <vector>
-#include <cmath>
 #include <set>
+#include <cmath>
+#include <iostream>
 
 using namespace std;
-
 int vis[10];
-int cnt=0;
-set<int> sosu;
+set<int> prime_numbers;
 
-bool check(int num){
-    if(num == 0 || num == 1) return false;
-    if(num == 2) return true;
-    
-    for(int i=2; i<=sqrt(num); i++){
+bool isPrime(int num){
+    if(num == 0 || num ==1) return false;
+    for(int i=2; i <= sqrt(num); i++){
         if(num%i==0) return false;
     }
     return true;
 }
 
-void dfs(string numbers, string num, int level){
-    if(num != "" && num[0] != '0'){
-        if(check(stoi(num))) {
-            sosu.insert(stoi(num));
-        }  
+void dfs(string num, string numbers){
+    cout << num << '\n';
+    if(num != "" && isPrime(stoi(num))){
+        prime_numbers.insert(stoi(num));
+        //cout << num << '\n';
     }
     
-    if(level == numbers.size()) return;
+    if(num.size() == numbers.size()) return;
     
     for(int i=0; i<numbers.size(); i++){
         if(vis[i]==0){
             vis[i]=1;
-            dfs(numbers, num+numbers[i] ,level+1);
+            num = num + numbers[i];
+            dfs(num, numbers);
+            num = num.substr(0,num.size()-1);
             vis[i]=0;
         }
     }
 }
-
 int solution(string numbers) {
     int answer = 0;
-    dfs(numbers, "", 0);
-    return sosu.size();
+    for(int i=0; i<numbers.size(); i++){
+        string num = "";
+        if(vis[i]==0){
+            vis[i]=1;
+            num = num + numbers[i];
+            dfs(num,numbers);
+            num = num.substr(0,num.size()-2);
+            vis[i]=0;
+        }
+    }    
+    return prime_numbers.size();
 }
