23bd24ac304acce2e0280622e138f8ff6da29d22
Kang Sumin <neoramiregi@gmail.com>
2024-08-29 13:59:51 +0900

    [level 2] Title: 피로도, Time: 17.28 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,32 +1,25 @@
 #include <string>
 #include <vector>
-#include <iostream>
 
 using namespace std;
+int max_cnt = -1;
+bool vis[10];
 
-int vis[10];
-int max_val = -1;
-void dfs(int k, vector<vector<int>> dungeons, int level){
-    if(level>max_val){
-        max_val = level;
-    }
-
+void dfs(int k, int cnt, vector<vector<int>> dungeons){
+    if(cnt > max_cnt) max_cnt = cnt;
+    if(cnt >= dungeons.size()) return;
+    
     for(int i=0; i<dungeons.size(); i++){
-        if(vis[i]==0 && dungeons[i][0]<= k){
+        if(vis[i]==0 && k >= dungeons[i][0]){
             vis[i]=1;
-            dfs(k-dungeons[i][1], dungeons, level+1);
+            dfs(k-dungeons[i][1],cnt+1,dungeons);
             vis[i]=0;
         }
     }
+    
+    
 }
-
 int solution(int k, vector<vector<int>> dungeons) {
-    dfs(k, dungeons, 0);
-    return max_val;
-}
-
-int main(){
-    int k = 80;
-    vector<vector<int>> dungeons = {{80,20},{50,40},{30,10}};
-    cout << solution(k, dungeons);
+    dfs(k,0,dungeons);
+    return max_cnt;
 }
