e8d8577465a62a73850a9e79d4ce26e3f92b20ca
Kang Sumin <neoramiregi@gmail.com>
2024-09-03 14:15:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 117.91 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -7,14 +7,20 @@ using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
     string answer = "";
     map<string,int> m;
-    for(int i=0; i<participant.size(); i++){
-        m[participant[i]] += 1;
+    for(auto participant_person:participant){
+        m[participant_person] += 1;
     }
-    for(int i=0; i<completion.size(); i++){
-        m[completion[i]] -= 1;
+    
+    for(auto completion_person:completion){
+        m[completion_person] -= 1;
     }
-    for(int i=0; i<participant.size(); i++){
-        if(m.find(participant[i])==m.end()) return  participant[i];
-        if(m[participant[i]] != 0) return participant[i];
+    
+    for(auto participant_person:participant){
+        if(m[participant_person]!=0){
+            answer = participant_person;
+            break;
+        }
+        
     }
+    return answer;
 }
