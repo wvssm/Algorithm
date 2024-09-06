f7c84740e5cb8a1d5ee564fe756e5264cd082764
Kang Sumin <neoramiregi@gmail.com>
2024-09-06 18:11:09 +0900

    [level 2] Title: 전화번호 목록, Time: 398.87 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,26 +1,27 @@
-// 우선 phone_book에 있는 전화번호들을 map에 담음
-// 다시 phone_book을 순회하면서 각 글자를 한 글자씩 더해서 hashmap에 존재하는지 확인
-// 해당 원래 글자와 같은 hashmap이 아니라면 false 반환
-
+/*
+1. 먼저 전화번호를 해시에 저장한다.
+2. 전화번호를 순회하면서, 한 자리씩 추가해보면서 이미 존재하는 전화번호인지 확인한다.
+시간복잡도는 O(전화번호 수*전화번호 길이)인데 전화번호수가 1000,000 이고, 전화번호 길이가 20이기 때문에 1초 안에 연산이 충분히 가능하다.
+*/
 #include <string>
 #include <vector>
 #include <unordered_map>
 
 using namespace std;
-
 unordered_map<string, int> um;
+
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-    for(auto phone:phone_book){
-        um[phone] = 1;
+    for(auto phone_num:phone_book){
+        um[phone_num] = 1;
     }
     
-    for(auto phone:phone_book){
-        string phone_num = "";
-        for(int i=0; i<phone.size(); i++){
-            phone_num = phone_num + phone[i];
-            if( um[phone_num]==1 && phone_num != phone){
+    for(auto phone_num:phone_book){
+        string num = "";
+        for(int i=0; i<phone_num.size(); i++){
+            num = num + phone_num[i];
+            if(um[num]==1 && num != phone_num){
                 return false;
             }
         }
