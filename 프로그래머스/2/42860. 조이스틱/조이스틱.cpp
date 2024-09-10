db7c63494065155a643ab79a33de7799d22311e4
Kang Sumin <neoramiregi@gmail.com>
2024-09-10 19:16:28 +0900

    [level 2] Title: 조이스틱, Time: 3.05 ms, Memory: 4.65 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,52 +1,57 @@
+// 알파벳 방향 -> 그리디 (이동이 적은 걸로)
+// 커서의 이동 -> 그리디로 하니까 케이스가 너무 많음,, 완탐으로 간다! BFS -> 최단거리 보장 BFS로간다
 #include <string>
 #include <vector>
-#include <iostream>
-#include "math.h"
 #include <queue>
 #include <tuple>
+
 using namespace std;
 
 int solution(string name) {
     int answer = 0;
-    string init="";
-    for(int i=0;i<name.size();i++){
-       init.append("A"); 
+    
+    // 초기 문자열 만들기
+    string start_str = "";
+    for(int i=0; i<name.size(); i++){
+        start_str += "A";
     }
     
-    queue<tuple<string,int,int> > q;
-    q.push(make_tuple(init,0,0));
+    queue<tuple<string, int, int>> q;
+    q.push(make_tuple(start_str,0,0));
     
-   	while(!q.empty()){
-        tuple<string,int,int> cur = q.front();
-       	q.pop();
+    while(!q.empty()){
+        auto node = q.front(); q.pop();
+        string now_str = get<0>(node);
+        int cur = get<1>(node);
+        int cnt = get<2>(node);
         
-        
-       	
-        string cur_string = get<0>(cur);
-        int cur_idx = get<1>(cur);
-        int cur_sum = get<2>(cur);
-        
-        if(cur_string[cur_idx] != name[cur_idx]){
-        	int dif_neg = 'A' - name[cur_idx] + 26;
-       		int dif_pos = name[cur_idx]-'A';
-    		int tmp = abs(dif_neg) > abs(dif_pos) ? abs(dif_pos) : abs(dif_neg);
-            cur_sum += tmp;
-            cur_string[cur_idx] = name[cur_idx];
-        }
-        if(cur_string == name){
-            cout << cur_string;
-            return cur_sum;
-        }
-        else{
-            int pos_idx = cur_idx + 1;
-            if(pos_idx >= name.size()) pos_idx = 0;
-            int neg_idx = cur_idx - 1;
-            if(neg_idx < 0) neg_idx = name.size()-1;
-            q.push(make_tuple(cur_string,pos_idx,cur_sum+1));
-            q.push(make_tuple(cur_string,neg_idx,cur_sum+1));
+        if(now_str[cur]!=name[cur]){
+            int right = name[cur]-now_str[cur];
+            int left = 26 - (name[cur]-now_str[cur]);
+            
+            if(right<left) cnt += right;
+            else cnt += left;
+            
+            now_str[cur] = name[cur];
         }
         
-    } 
+        if(now_str == name){
+            return cnt;
+        }
+        
+        int plus = cur + 1;
+        if(plus > name.size() - 1){
+            plus = plus - name.size();
+        }
+        
+        int minus = cur - 1;
+        if(minus < 0){
+            minus = name.size() + minus;
+        }
+        
+        q.push(make_tuple(now_str,plus,cnt+1));
+        q.push(make_tuple(now_str,minus,cnt+1));
+    }
     
     return answer;
 }
