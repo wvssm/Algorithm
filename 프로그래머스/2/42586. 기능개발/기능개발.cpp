80fd792eb7471adceacad3bd1ed9f27aaee86eac
Kang Sumin <neoramiregi@gmail.com>
2024-09-12 22:06:47 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -3,34 +3,41 @@
 #include <stack>
 #include <iostream>
 
-// stack 사용
-// 진척도를 역순으로 stack에 담음
-// st.top() 진척도가 100넘은지 확인, 계속 pop, pop한 개수 answer에 저장
-// stack이 빌 때까지 반복
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    stack<pair<int,int>> st;
-    int cnt = 0;
+    stack<pair<int, int>> st;
     int day = 0;
+    int progress = 0;
+    int speed = 0;
+    int now_progress = 0;
+    int cnt = 0;
     
     for(int i=speeds.size()-1; i>=0; i--){
         st.push({progresses[i], speeds[i]});
     }
-
+    
     while(!st.empty()){
+        progress = st.top().first; 
+        speed = st.top().second;
+        now_progress = (100 - progress) - day * speed;
         cnt = 0;
-        if((100-st.top().first - day* st.top().second)%st.top().second==0) day += (100-st.top().first - day* st.top().second)/st.top().second;
-        else day += (100-st.top().first - day* st.top().second)/st.top().second + 1;
         
-        while(st.top().first + st.top().second * day >= 100){
-            cnt++;
+        if(now_progress % speed == 0){
+            day += now_progress / speed;
+        } else {
+            day += now_progress / speed + 1;
+        }
+        
+        while (progress + day * speed >= 100){
             st.pop();
+            cnt++;
             if(st.empty()) break;
+            progress = st.top().first;
+            speed = st.top().second;
         }
         answer.push_back(cnt);
     }
-    
     return answer;
 }
