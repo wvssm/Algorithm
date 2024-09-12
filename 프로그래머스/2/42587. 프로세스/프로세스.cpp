a89f6183bf69855f3d3a11a36872c42e4e68643a
Kang Sumin <neoramiregi@gmail.com>
2024-09-12 22:43:46 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,22 +1,36 @@
+// priorities copy, 오름차순으로 정렬
+
 #include <string>
 #include <vector>
+#include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
     int answer = 0;
-    while(true){
-        int m_e = *max_element(priorities.begin(),priorities.end());
-        if(priorities[0]!=m_e) {
-            priorities.push_back(priorities[0]);
-            if(location==0) location = priorities.size()-1;
-        }
-        else{
-            answer++;
-            if(location==0) return answer;
-        }
-        priorities.erase(priorities.begin());
-        location--;
+    
+    vector<int> my_prior = priorities;
+    sort(my_prior.begin(), my_prior.end(), greater<>());
+    
+    vector<pair<int,int>> q;
+    for(int i=0; i< priorities.size(); i++){
+        q.push_back({priorities[i] ,i});
     }
+    
+    int prior_cur = 0;
+    int cur = 0;
+    
+    while(prior_cur < my_prior.size()){
+        if(my_prior[prior_cur] == q[cur].first){
+            if(q[cur].second == location) return prior_cur+1;
+            cur++;
+            prior_cur++;
+        } else{
+            q.push_back(q[cur]);
+            cur++;
+        }
+    }
+    
     return answer;
 }
