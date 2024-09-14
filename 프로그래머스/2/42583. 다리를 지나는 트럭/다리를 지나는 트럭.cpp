c7c75c610edda14cca6da091314ecbaf48b97567
Kang Sumin <neoramiregi@gmail.com>
2024-09-14 18:09:35 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.41 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -1,35 +1,42 @@
-#include <string>
-#include <vector>
-#include <queue>
 #include <iostream>
-
+#include<algorithm>
+#include <functional>         // greater 사용 위해 필요  
+#include <vector>
+#include<queue>
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    queue<int> q;
-    int sum = 0;
-    int cnt = 1;
-    int cur = 0;
-    
-    while(true){
-        if(q.size()>=bridge_length){
-            sum -= q.front();
-            if(q.front() == truck_weights[truck_weights.size()-1] && cur == truck_weights.size() && sum == 0){
-                return cnt;
+    int answer = 0;
+    int count = 0;
+    int Time = 0; 
+    int Truck_weight = 0;
+    queue<pair<int, int>> truck_move;
+
+    while (true)
+    {
+        if (weight >= Truck_weight + truck_weights.at(count))
+        {
+            truck_move.push(make_pair(truck_weights.at(count), bridge_length + 1 + Time));
+            Truck_weight += truck_weights.at(count);
+            count++;
+        }
+
+        if (count >= truck_weights.size())
+        {
+            answer = truck_move.back().second;
+            break;
+        }
+        else
+        {
+            Time++;
+            if (truck_move.front().second == Time+1)
+            {
+                Truck_weight -= truck_move.front().first;
+                truck_move.pop();
             }
-            q.pop();
         }
-        
-        
-        if(cur < truck_weights.size() && sum + truck_weights[cur] <= weight){
-            q.push(truck_weights[cur]);
-            sum = sum + truck_weights[cur];
-            cur++;
-        } else{
-            q.push(0);
-        }
-        cnt++;   
+
     }
-    
-    return cnt;
+
+    return answer;
 }
