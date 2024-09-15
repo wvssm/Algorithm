c274046c7b01501de7131f483d594ee390e1074d
Kang Sumin <neoramiregi@gmail.com>
2024-09-15 16:15:41 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.32 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -5,29 +5,28 @@
 using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
+    queue<pair<int,int>> q;
     int answer = 0;
-    int time = 0;
     int total_weight = 0;
+    int time = 0;
     int cur = 0;
     
-    queue<pair<int,int>> q;
-    
     while(true){
-        if(weight >= total_weight + truck_weights.at(cur)){
-            total_weight += truck_weights.at(cur);
-            q.push({truck_weights.at(cur), time + bridge_length + 1});
+        if(weight >= total_weight + truck_weights[cur]){
+            q.push({truck_weights[cur], time + bridge_length + 1});
+            total_weight += truck_weights[cur];
             cur++;
         }
         
         if(cur >= truck_weights.size()){
             return q.back().second;
-        }
-        else{
-            time++;
-            if(q.front().second == time + 1){
-                total_weight -= q.front().first;
-                q.pop();
-            }
+        } 
+        
+        time++;
+        
+        if(time + 1 == q.front().second){
+            total_weight -= q.front().first;
+            q.pop();
         }
     }
     return answer;
