5956d2ecaedfe4e47d61dacb2905ecb3e565fd25
Kang Sumin <neoramiregi@gmail.com>
2024-11-25 17:20:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.48 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
 
 ### 성능 요약
 
-메모리: 32.9 MB, 시간: 117.91 ms
+메모리: 95.5 MB, 시간: 84.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 03일 14:15:43
+2024년 11월 25일 17:20:52
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,31 @@
+/*
+풀이 방법
+map을 이용해여 participant을 순회하면서 사람이름을 key 그리고 숫자를 1씩 높인다.
+completion을 순회하여 사람이름을 key로 하여 숫자를 1씩 뺀다. 
+숫자가 1이상이면 완주하지 못한 사람 이름을 반환한다.
+*/
+
+import java.util.HashMap;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> personMap = new HashMap<>();
+        
+        for(String person : participant){
+            personMap.put(person, personMap.getOrDefault(person, 0)+1);
+        }
+        
+        for(String person : completion){
+            personMap.put(person, personMap.getOrDefault(person, 0)-1);
+        }
+        
+        for(String person : participant){
+            if(personMap.get(person)!=0){
+                return person;
+            }
+        }
+        
+        return answer;
+    }
+}
