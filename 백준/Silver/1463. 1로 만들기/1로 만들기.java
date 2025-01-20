2ec57cb0978cc0651fbd249d245c1d46e6eba071
Kang Sumin <neoramiregi@gmail.com>
2025-01-20 22:06:33 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18128 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -16,16 +16,13 @@ class Main{
         dp[1] = 0;
 
         for(int i=2; i<=n; i++){
-            if(i%6==0){
-                dp[i] = Math.min(dp[i/3], Math.min(dp[i/2], dp[i-1])) + 1;
+            dp[i] = dp[i-1] + 1;
+            if(i%3 == 0){
+                dp[i] = Math.min(dp[i/3]+1, dp[i]);
             }
-            else if(i%3==0){
-                dp[i] = Math.min(dp[i/3], dp[i-1]) + 1;
-            } else if (i%2==0){
-                dp[i] = Math.min(dp[i/2], dp[i-1]) + 1;
-            } else{
-                dp[i] = dp[i-1] + 1;
-            }
+            if (i%2 == 0){
+                dp[i] = Math.min(dp[i/2]+1, dp[i]);
+            } 
         }
 
         bw.append(String.valueOf(dp[n]));
