8ba47717af176da09e64f999b2f6c3d65e739fb1
Kang Sumin <neoramiregi@gmail.com>
2025-01-20 22:03:58 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18228 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Main{
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int []dp = new int [n+1];
+
+        dp[1] = 0;
+
+        for(int i=2; i<=n; i++){
+            if(i%6==0){
+                dp[i] = Math.min(dp[i/3], Math.min(dp[i/2], dp[i-1])) + 1;
+            }
+            else if(i%3==0){
+                dp[i] = Math.min(dp[i/3], dp[i-1]) + 1;
+            } else if (i%2==0){
+                dp[i] = Math.min(dp[i/2], dp[i-1]) + 1;
+            } else{
+                dp[i] = dp[i-1] + 1;
+            }
+        }
+
+        bw.append(String.valueOf(dp[n]));
+        bw.flush();
+        bw.close();
+    }
+}
