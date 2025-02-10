9cd34ac1ea63e0e446e037a7225adc7f232d1353
Kang Sumin <neoramiregi@gmail.com>
2025-02-10 14:46:29 +0900

    [Silver III] Title: 프린터 큐, Time: 132 ms, Memory: 17100 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int test_case = Integer.parseInt(st.nextToken());
+        
+        for(int i=0; i<test_case; i++) {
+        	st = new StringTokenizer(br.readLine());
+        	int n = Integer.parseInt(st.nextToken());
+        	int m = Integer.parseInt(st.nextToken());
+        	
+        	int []arr = new int[n];
+        	
+        	Queue<int[]> q = new LinkedList<>();
+        	st = new StringTokenizer(br.readLine());
+        	for(int j=0; j<n; j++) {
+        		int val = Integer.parseInt(st.nextToken());
+        		q.add(new int[] {val,j});
+        		arr[j] = val;
+        	}
+        	
+        	Arrays.sort(arr);
+        	
+        	int cnt = 0;
+        	while(true) {
+        		int num = q.peek()[0];
+        		int pos = q.peek()[1];
+        		
+        		if(num == arr[n-1]) {
+        			cnt++;
+        			q.poll();
+        			arr[n-1]= -1;
+        			Arrays.sort(arr);
+        			if(pos == m) {
+        				bw.append(String.valueOf(cnt)+'\n');
+        				break;
+        			}
+        		} else {
+        			int[] small = q.poll();
+        			q.add(small);
+        		}
+        	}
+        }
+        bw.flush();
+        bw.close();
+        
+    }
+}
