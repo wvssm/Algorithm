e59cfda02243b5e4538aa80e5bb71d3f422f1ab6
Kang Sumin <neoramiregi@gmail.com>
2025-02-18 23:15:18 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -10,19 +10,14 @@ class Main{
 
         int num = Integer.parseInt(st.nextToken());
 
-        int i = 0;
-        int sixDivide = 0;
+        int distance = 1;
+        int curMax = 1;
 
-        while(true){
-            sixDivide += i;
-            
-            if(num <= sixDivide * 6 + 1){ // 기준 수 보다 작거나 같으면
-                System.out.println(i+1);
-                break;
-            }
-
-            i++;
+        while(num > curMax){
+            curMax += 6 * distance;
+            distance++;
         }
+        System.out.println(distance);
 
     }
 }
