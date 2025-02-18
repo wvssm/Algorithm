eee04a3dbd1587254321dda9dca9418c1669b7de
Kang Sumin <neoramiregi@gmail.com>
2025-02-18 21:27:47 +0900

    [Silver II] Title: 마인크래프트, Time: 676 ms, Memory: 34108 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.java
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -1,15 +1,17 @@
 import java.util.*;
 import java.io.*;
 
-class Main {
+class Main
+{
     static int N, M, B, time, block, min, max, min_time, height;
-    static int[][] map;
+    static int [][] map;
 
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        // 입력 받기
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
@@ -20,40 +22,48 @@ class Main {
         min_time = Integer.MAX_VALUE;
         height = 0;
 
-        for (int i = 0; i < N; i++) {
+        for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for(int j=0; j<M; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                min = Math.min(min, map[i][j]);
-                max = Math.max(max, map[i][j]);
+                if(min>map[i][j]) {
+                    min = map[i][j];
+                }
+                if(max<map[i][j]) {
+                    max = map[i][j];
+                }
             }
         }
 
-        for (int i = min; i <= max; i++) {
+        // 가장 낮은 블럭부터 가장 높은 블럭까지 시간 구해보기
+        for(int i=min; i<=max; i++) {
+
             block = B;
             time = 0;
-
-            for (int j = 0; j < N; j++) {
-                for (int k = 0; k < M; k++) {
+            for(int j=0; j<N; j++) {
+                for(int k=0; k<M; k++) {
                     int diff = map[j][k] - i;
 
-                    if (diff > 0) { // 블록 제거
-                        time += diff * 2;
-                        block += diff;
-                    } else if (diff < 0) { // 블록 추가
+                    if(diff< 0) {
                         time += Math.abs(diff);
                         block -= Math.abs(diff);
+                    } else if(diff >0) {
+                        time += diff * 2;
+                        block += diff;
                     }
                 }
             }
 
-            if (block >= 0 && (min_time > time || (min_time == time && height < i))) {
-                min_time = time;
-                height = i;
+            if(block >= 0) {
+                if(min_time > time || (height < i && min_time == time)) {
+                    min_time = time;
+                    height = i;
+                }
             }
         }
 
-        bw.write(min_time + " " + height);
+
+        bw.append(min_time + " " + height);
         bw.flush();
         bw.close();
     }
