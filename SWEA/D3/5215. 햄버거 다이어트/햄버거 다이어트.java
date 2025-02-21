5f6389d9cd3cd29aed8510e44f6090888ec0a618
Kang Sumin <neoramiregi@gmail.com>
2025-02-21 12:29:16 +0900

    [D3] Title: 햄버거 다이어트, Time: 122 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,59 +1,57 @@
+//package test;
+
 import java.util.*;
 import java.io.*;
 
-/*
-   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
-   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
- */
-class Solution
-{
-	static int nums[];
-	static int arr[][];
-	static int n, l, max;
-	public static void main(String args[]) throws Exception
-	{
+
+class Solution{
+	static int T;
+	static int N, L, max;
+	static int [][]arr;
+	static StringTokenizer st;
+	
+	public static void main(String args[]) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T;
+		st = new StringTokenizer(br.readLine());
+		
 		T = Integer.parseInt(st.nextToken());
-
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+		
+		for(int test_case = 1; test_case <= T; test_case++){
 			st = new StringTokenizer(br.readLine());
-			n = Integer.parseInt(st.nextToken());
-			l = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			L = Integer.parseInt(st.nextToken());
 			
-			arr = new int[n][2];
-			nums = new int[n];
+			arr = new int[N][2];
 			
-			for(int i=0; i<n; i++) {
+			for(int i=0; i<N; i++) {
 				st = new StringTokenizer(br.readLine());
 				arr[i][0] = Integer.parseInt(st.nextToken());
 				arr[i][1] = Integer.parseInt(st.nextToken());
 			}
 			
 			max = 0;
-			
 			combi(0,0,0,0);
 			
-			System.out.println("#"+test_case+" "+max);
+			bw.append("#" + test_case + " " + max + "\n");
 		}
+			
+		
+		bw.flush();
+		bw.close();
 	}
 	
-	static void combi(int depth, int start, int like, int cal) {
-		if(cal <= l) {
-			if(max < like) {
-				max = like;
-			}
+	static void combi(int depth, int start, int cal, int like) {
+		if(cal <= L && max < like) {
+			max = like;
 		}
 		
-		if(depth>=n) {
+		if(cal >= L || depth >= N ) {
 			return;
 		}
 		
-		for(int i=start; i<n; i++) {
-			combi(depth+1, i+1, like+arr[i][0], cal+arr[i][1]);
+		for(int i=start; i<N; i++) {
+			combi(depth+1, i+1, cal + arr[i][1], like + arr[i][0]);
 		}
 	}
 }
