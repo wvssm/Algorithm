8c9f41f8f007b8bce2e77b0e477b0772b2f79931
Kang Sumin <neoramiregi@gmail.com>
2025-02-25 22:35:00 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 120 ms, Memory: 14888 KB -BaekjoonHub

--- 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -2,20 +2,53 @@
 import java.io.*;
 import java.util.*;
 
-// nCr
-
 class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
     static int N, M;
     static ArrayList<ArrayList<Integer>> bridge;
     static boolean[] vis;
-    static int answer;
-    static int cnt, sum_cnt;
-    static int min_cnt;
+    static int node_index, bacon, min_bacon;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+
+        input();
+
+        min_bacon = Integer.MAX_VALUE;
+        for(int i=1; i<=N; i++) {
+            vis = new boolean[N+1];
+            bacon = 0;
+            bfs(i);
+
+            if(min_bacon > bacon) {
+                min_bacon = bacon;
+                node_index = i;
+            }
+        }
+        System.out.println(node_index);
+    }
+
+    static void bfs(int start) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start,0});
+        vis[start] = true;
+
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            bacon += cur[1];
+
+            for(int node : bridge.get(cur[0])){
+                if(vis[node]) continue;
+
+                vis[node] = true;
+                q.add(new int[]{node, cur[1]+1});
+            }
+        }
+    }
+    
+    static void input() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
@@ -31,38 +64,9 @@ class Main {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
+
             bridge.get(start).add(end);
             bridge.get(end).add(start);
         }
-
-        min_cnt = Integer.MAX_VALUE;
-        for(int i=1; i<=N; i++) {
-            vis = new boolean[N+1];
-            cnt = 0;
-            bfs(i);
-
-            if(min_cnt > cnt) {
-                min_cnt = cnt;
-                answer = i;
-            }
-        }
-        System.out.println(answer);
-    }
-
-    static void bfs(int start) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{start,0});
-        vis[start] = true;
-
-        while(!q.isEmpty()){
-            int[] cur = q.poll();
-            cnt += cur[1];
-            for(int node : bridge.get(cur[0])){
-                if(vis[node]) continue;
-                vis[node] = true;
-                q.add(new int[]{node, cur[1]+1});
-            }
-        }
-
     }
 }
