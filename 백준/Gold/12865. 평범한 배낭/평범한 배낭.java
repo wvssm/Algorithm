65923fccc3e9e1f449aaaa2f3934055418cf5d56
Kang Sumin <neoramiregi@gmail.com>
2025-04-24 21:47:42 +0900

    [Gold V] Title: 평범한 배낭, Time: 168 ms, Memory: 53736 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,41 +1,44 @@
 import java.io.*;
 import java.util.*;
 
-class Main{		
-	public static void main(String args[])throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int n = Integer.parseInt(st.nextToken());
-		int k = Integer.parseInt(st.nextToken());
-		
-		int [] w = new int[n+1];
-		int [] v = new int[n+1];
-		
-		int [][]dp = new int[n+1][k+1];
-		
-		for(int i=1; i<=n; i++) {
-			st = new StringTokenizer(br.readLine());
-			w[i] = Integer.parseInt(st.nextToken());
-			v[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int max_val = 0;
-		for(int i=1; i<=n; i++) {
-			for(int j=1; j<=k; j++) {
-				dp[i][j] = dp[i-1][j];
-				if(j-w[i]>=0) {
-					dp[i][j] = Math.max(dp[i][j], dp[i-1][j-w[i]] + v[i]);
-					if(max_val < dp[i][j]) {
-						max_val = dp[i][j];
-					}
-				}
-			}
-		}
-		
-		bw.append(String.valueOf(max_val));
-		bw.flush();
-		bw.close();
-	}
+class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static int[][] arr;
+    static int[][] dp;
+    static int N, K;
+
+    public static void main(String[] args) throws Exception {
+        input();
+
+        dp = new int[N+1][K+1];
+
+        for(int i=1; i<=N; i++){
+            for(int j=1; j<=K; j++){
+                if(arr[i][0] > j){
+                    dp[i][j] = dp[i-1][j];
+                } else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-arr[i][0]] + arr[i][1]);
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+
+    public static void input() throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine().trim());
+
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        arr = new int[N+1][2];
+
+        for(int i=1; i<=N; i++){
+            st = new StringTokenizer(br.readLine().trim());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+    }
 }
